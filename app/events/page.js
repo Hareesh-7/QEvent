ecb59bc94c0f9ab3457e3703f6e2f4997ec72841
Hareesh-7 <m.hareesh2504@gmail.com>
2024-09-20 12:05:07 +0530

    Events

---
+++ app/events/page.js
@@ -0,0 +1,19 @@
+
+import EventCard from "@/components/EventCard";
+export default async function EventsPage(){
+   const response= await fetch("https://qevent-backend.labs.crio.do/events") ;
+   const events= await response.json();
+   
+    return (
+        
+        <div className="w-full flex flex-wrap justify-between">
+            {events.map(event=>{
+                return(
+                    <EventCard eventData={event}/>
+                )
+            })}
+        </div>
+        
+        
+    )
+}
