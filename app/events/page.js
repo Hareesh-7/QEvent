8a6a7b657fd6a08bc3597c50d3f20c13363c0451
Hareesh-7 <m.hareesh2504@gmail.com>
2024-09-20 15:13:42 +0530

    events for each artist (milestone-3)

--- app/events/page.js
+++ app/events/page.js
@@ -1,19 +1,28 @@
-
+'use client';
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 import EventCard from "@/components/EventCard";
-export default async function EventsPage(){
-   const response= await fetch("https://qevent-backend.labs.crio.do/events") ;
-   const events= await response.json();
-   
+
+async function EventsComponent() {
+    const searchParam = useSearchParams();
+    const response = await fetch("https://qevent-backend.labs.crio.do/events");
+    let events = await response.json();
+    if (searchParam?.get('artist'))
+        events = events.filter(event => event.artist === searchParam.get('artist'))
+    if (searchParam?.get('tag'))
+        events = events.filter(event => event.tags.includes(searchParam.get('tag')));
+
     return (
-        
-        <div className="w-full flex flex-wrap justify-between">
-            {events.map(event=>{
-                return(
-                    <EventCard eventData={event}/>
-                )
-            })}
+        <div className="flex flex-row gap-x-2 gap-y-2 px-3 py-3 flex-wrap justify-evenly">
+            {events.map(event => (<EventCard eventData={event} />))}
         </div>
-        
-        
     )
+}
+
+export default function EventsPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <EventsComponent />
+        </Suspense>
+      );
 }
